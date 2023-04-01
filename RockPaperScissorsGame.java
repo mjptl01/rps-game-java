d8153b28113932e8a1f905b084aedf3318793424
Mihir Patel <mihirpatel542001@gmail.com>
2023-04-01 14:12:17 -0400

    Add driver code in main class

--- RockPaperScissorsGame.java
+++ RockPaperScissorsGame.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 /**
  * The driver class with main method.
  */
 public class RockPaperScissorsGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GameLogic gameLogic = new GameLogic(scanner);
+        GameUI gameUI = new GameUI(scanner, gameLogic);
 
+        gameUI.runGame();
     }
 }
